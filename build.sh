83e7cae59bc3fa65fc2d58d0c4758443a09e213d
whismanoid <mark_underwood@sonic.net>
2023-06-01 02:13:22 -0700

    attempting to test MAX3421-HCD device driver; not deployed?

---
+++ build.sh
@@ -0,0 +1,7 @@
+make -j4 zImage modules dtbs
+sudo make modules_install
+printf "copying boot files\n"
+sudo cp arch/arm/boot/dts/*.dtb /boot/
+sudo cp arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/
+sudo cp arch/arm/boot/dts/overlays/README /boot/overlays/
+sudo cp arch/arm/boot/zImage /boot/$KERNEL.img
