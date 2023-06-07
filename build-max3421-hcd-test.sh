abd5efb1557b35823b9222032bf7760a40148fc5
whismanoid <mark_underwood@sonic.net>
2023-06-06 23:22:14 -0700

    MAX3421-HCD USB host testing on RPi 3 A+/B+; WIP

---
+++ build-max3421-hcd-test.sh
@@ -0,0 +1,23 @@
+printf "make -j4 zImage modules dtbs\n"
+make -j4 zImage modules dtbs
+printf "make modules_install\n"
+sudo make modules_install
+printf "copying boot files\n"
+printf "sudo cp arch/arm/boot/dts/*.dtb /boot/\n"
+sudo cp arch/arm/boot/dts/*.dtb /boot/
+printf "sudo cp arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/\n"
+sudo cp arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/
+printf "sudo cp arch/arm/boot/dts/overlays/README /boot/overlays/\n"
+sudo cp arch/arm/boot/dts/overlays/README /boot/overlays/
+printf "sudo cp arch/arm/boot/zImage /boot/$KERNEL.img\n"
+sudo cp arch/arm/boot/zImage /boot/$KERNEL.img
+#
+# sudo cp drivers/usb/host/max3421-hcd.ko /lib/modules/$(uname -r)
+# assuming VERSION=6 PATCHLEVEL=1 SUBLEVEL=31
+# then install in 6.1.31-v7+
+printf "sudo cp drivers/usb/host/max3421-hcd.ko /lib/modules/6.1.31-v7+\n"
+sudo cp drivers/usb/host/max3421-hcd.ko /lib/modules/6.1.31-v7+
+printf "find /lib/modules -type f -name '*.ko' | grep -i max3421-hcd.ko"
+find /lib/modules -type f -name '*.ko' | grep -i max3421-hcd.ko
+printf "sudo modprobe max3421-hcd"
+sudo modprobe max3421-hcd
--- build.sh
+++
@@ -1,8 +0,0 @@
-make -j4 zImage modules dtbs
-sudo make modules_install
-printf "copying boot files\n"
-sudo cp arch/arm/boot/dts/*.dtb /boot/
-sudo cp arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/
-sudo cp arch/arm/boot/dts/overlays/README /boot/overlays/
-sudo cp arch/arm/boot/zImage /boot/$KERNEL.img
-sudo cp drivers/usb/host/max3421-hcd.ko /lib/modules/$(uname -r)
